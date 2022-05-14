3e5309b02e60b1e8b120ff21021e02bcb9562710
Jihoon Song <jihoonsong@Jihoons-MacBook-Pro.local>
2022-05-14 11:02:20 +0200

    Add ContractError

---
+++ contracts/lending/src/error.rs
@@ -0,0 +1,11 @@
+use cosmwasm_std::{OverflowError, StdError};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ContractError {
+    #[error("{0}")]
+    Std(#[from] StdError),
+
+    #[error("{0}")]
+    OverflowError(#[from] OverflowError),
+}
