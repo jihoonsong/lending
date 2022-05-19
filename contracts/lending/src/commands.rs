4638bfb654724986acaa87c67314775d52f8657d
Jihoon Song <jihoonsong@Jihoons-MacBook-Pro.local>
2022-05-19 23:46:15 +0900

    Add make_borrow_request()

--- contracts/lending/src/commands.rs
+++ contracts/lending/src/commands.rs
@@ -1,1 +1,25 @@
+use astroport::asset::Asset;
+use cosmwasm_std::{CanonicalAddr, DepsMut, Response};
 
+use crate::error::ContractError;
+
+/// ## Description
+/// Make borrow request
+/// Returns [`Response`] with specified attributes and messages if operation was successful
+/// Otherwise returns [`ContractError`]
+/// ## Params
+/// * **deps** is an object of type [`DepsMut`]
+///
+/// * **borrower** is an object of type [`CanonicalAddr`]
+///
+/// * **collateral** is an object of type [`Asset`]
+///
+/// * **period** is [`u64`]
+pub fn make_borrow_request(
+    _deps: DepsMut,
+    _borrower: &CanonicalAddr,
+    _collateral: Asset,
+    _period: u64,
+) -> Result<Response, ContractError> {
+    Ok(Response::new())
+}
