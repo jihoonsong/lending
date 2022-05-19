6e59e4018a13f8c0759cdaf6205ddc4a53464d33
Jihoon Song <jihoonsong@Jihoons-MacBook-Pro.local>
2022-05-19 22:08:00 +0900

    Add execute messages for making borrow request

--- packages/services/src/lending.rs
+++ packages/services/src/lending.rs
@@ -1,12 +1,32 @@
+use cw20::Cw20ReceiveMsg;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {}
 
+/// ## ExecuteMsg
+/// This structure describes execute messages
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    /// ## Description
+    /// Make borrow request with CW20 token
+    MakeBorrowRequestCw20Token(Cw20ReceiveMsg),
+    /// ## Description
+    /// Make borrow request with native token
+    MakeBorrowRequestNativeToken { period: u64 },
+}
+
+/// ## Cw20HookMsg
+/// This structure describes the CW20 hook message
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum Cw20HookMsg {
+    /// ## Description
+    /// Make borrow request
+    MakeBorrowRequest { period: u64 },
+}
 
 /// ## Description
 /// This structure describes query messages
